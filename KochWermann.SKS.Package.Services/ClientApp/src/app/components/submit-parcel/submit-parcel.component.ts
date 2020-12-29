171d3c357ed557823ecb9cd23736af5923d83bb7
Josef Koch <josef.koch97@gmail.com>
2020-12-29 11:31:54 +0100

    Added angular frontend

---
+++ KochWermann.SKS.Package.Services/ClientApp/src/app/components/submit-parcel/submit-parcel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-submit-parcel',
+  templateUrl: './submit-parcel.component.html',
+  styleUrls: ['./submit-parcel.component.scss']
+})
+export class SubmitParcelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ KochWermann.SKS.Package.Services/ClientApp/src/app/services/parcelApi.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Parcel} from '../models/parcel';
+import {Observable} from 'rxjs';
+import {NewParcelInfo} from '../models/newParcelInfo';
+import {TrackingInformation} from '../models/trackingInformation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ParcelApiService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  postParcel(parcel: Parcel): Observable<NewParcelInfo> {
+    return this.http.post<NewParcelInfo>(`${this.apiUrl}/parcel`, parcel);
+  }
+
+  getTrackingInformation(trackingId: string): Observable<TrackingInformation> {
+    return this.http.get<TrackingInformation>(`${this.apiUrl}/${trackingId}`);
+  }
+
+  reportDelivery(trackingId: string): Observable<string> {
+    return this.http.post<string>(`${this.apiUrl}/${trackingId}/reportDelivery`, null);
+  }
+
+  reportHop(trackingId: string, hopCode: string): Observable<string> {
+    return this.http.post<string>(`${this.apiUrl}/${trackingId}/reportHop/${hopCode}`, null);
+  }
+
+}
